9b37d3ce493e89983dd4b0225a1a9750699e139f
Tommy Leung <me@supertommy.com>
2014-06-28 11:57:43 -0400

    Initial Commit

---
+++ craft/craft.h
@@ -0,0 +1,19 @@
+//
+//  craft.h
+//  craft
+//
+//  Created by Tommy Leung on 6/28/14.
+//  Copyright (c) 2014 Tommy Leung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for craft.
+FOUNDATION_EXPORT double craftVersionNumber;
+
+//! Project version string for craft.
+FOUNDATION_EXPORT const unsigned char craftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <craft/PublicHeader.h>
+
+
---
+++ craftTests/craftTests.swift
@@ -0,0 +1,35 @@
+//
+//  craftTests.swift
+//  craftTests
+//
+//  Created by Tommy Leung on 6/28/14.
+//  Copyright (c) 2014 Tommy Leung. All rights reserved.
+//
+
+import XCTest
+
+class craftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
